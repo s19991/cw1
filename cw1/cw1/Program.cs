3881c89822c4e5b11616bdd5dc406dfd90b3373b
Fabian Schmidt <s19991@pjwstk.edu.pl>
2020-03-05 11:11:50 +0100

    dodalem pobieranie strony i czytanie maili regexem

---
+++ cw1/cw1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cw1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            args = new[] { "https://www.pja.edu.pl" };
+            // var to skrocony zapis dla HttpClient jak w javie
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(args[0]);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string html = await response.Content.ReadAsStringAsync();
+                var emailRegex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+");
+                var matches = emailRegex.Matches(html);
+                foreach(var match in matches)
+                {
+                    Console.WriteLine(match);
+                }
+            }
+
+
+            Console.WriteLine("End");
+        }
+    }
+}
